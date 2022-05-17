ff9dc2e53af8e555f0da220cdc9a2909aefe9e89
OgutuJoseph <onyango.joseph@yahoo.com>
2022-05-17 06:14:39 +0300

    Firebase Authentication

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
+import './Login.scss';
 
 const Login = () => {
+
+    const [error, setError] = useState(false);
+
     return (
-        <div>Login</div>
+        <div className='login'>
+            <form >
+                <input type='email' placeholder='email' />
+                <input type='password' placeholder='password' />
+                <button type='submit'>Login</button>
+                {error && <span>Wrong email or password!</span> }
+            </form>
+        </div>
     )
 }
 
